a6a1e50a02aa873c4eef7dc002e708fd135757d7
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-19 20:19:36 +0800

    Initial Commit

---
+++ SwiftUI DatePicker/SwiftUI_DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DatePickerApp.swift
+//  SwiftUI DatePicker
+//
+//  Created by Amben on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
